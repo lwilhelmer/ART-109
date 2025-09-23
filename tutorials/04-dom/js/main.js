bbc64a3c4b45a0a86cb31a13f66282db06f30ea1
Lea Wilhelmer <lea.wilhelmer@sjsu.edu>
2025-09-23 13:34:06 -0700

    moon img appear

---
+++ tutorials/04-dom/js/main.js
@@ -0,0 +1,28 @@
+
+document.querySelector("#img-01").addEventListener("click", function(){
+    document.querySelector("#img-02").style.visibility = "visible";
+    alert("bark bark bark");
+})
+
+document.querySelector("#img-02").addEventListener("click", function(){
+    document.querySelector("#img-03").style.visibility = "visible";
+})
+
+document.querySelector("#img-03").addEventListener("click", function(){
+    document.querySelector("#img-04").style.visibility = "visible";
+})
+
+document.querySelector("#img-04").addEventListener("click", function(){
+    document.querySelector("#img-05").style.visibility = "visible";
+})
+
+document.querySelector("#img-05").addEventListener("click", function(){
+    document.querySelector("#img-06").style.visibility = "visible";
+})
+
+document.querySelector("#img-06").addEventListener("click", function(){
+    document.querySelector("#img-07").style.visibility = "visible";
+})
+
+document.querySelector("#img-07").addEventListener("click", function(){
+})
