189091a716802e4bb3264eba0b5be908a46c17be
Lea Wilhelmer <lea.wilhelmer@sjsu.edu>
2025-09-25 10:16:18 -0700

    added images and make them appear

--- tutorials/04-dom/js/main.js
+++ tutorials/04-dom/js/main.js
@@ -1,28 +1,49 @@
+let img0 = document.querySelector("#img-0");
+let img1 = document.querySelector("#img-1");
+let img2 = document.querySelector("#img-2");
+let img3 = document.querySelector("#img-3");
+let img4 = document.querySelector("#img-4");
+let img5 = document.querySelector("#img-5");
+let img6 = document.querySelector("#img-6");
+let img7 = document.querySelector("#img-7");
+let img8 = document.querySelector("#img-8");
 
-document.querySelector("#img-01").addEventListener("click", function(){
-    document.querySelector("#img-02").style.visibility = "visible";
-    alert("bark bark bark");
-})
 
-document.querySelector("#img-02").addEventListener("click", function(){
-    document.querySelector("#img-03").style.visibility = "visible";
-})
 
-document.querySelector("#img-03").addEventListener("click", function(){
-    document.querySelector("#img-04").style.visibility = "visible";
-})
 
-document.querySelector("#img-04").addEventListener("click", function(){
-    document.querySelector("#img-05").style.visibility = "visible";
-})
+img0.addEventListener('click', () => {
+    img3.classList.remove("hidden");
+});
 
-document.querySelector("#img-05").addEventListener("click", function(){
-    document.querySelector("#img-06").style.visibility = "visible";
-})
 
-document.querySelector("#img-06").addEventListener("click", function(){
-    document.querySelector("#img-07").style.visibility = "visible";
-})
+img3.addEventListener('click', () => {
+    img7.classList.remove("hidden");
+});
 
-document.querySelector("#img-07").addEventListener("click", function(){
-})
+
+img7.addEventListener('click', () => {
+    img2.classList.remove("hidden");
+});
+
+img2.addEventListener('click', () => {
+    img8.classList.remove("hidden");
+});
+
+img8.addEventListener('click', () => {
+    img1.classList.remove("hidden");
+});
+
+img1.addEventListener('click', () => {
+    img4.classList.remove("hidden");
+});
+
+img4.addEventListener('click', () => {
+    img6.classList.remove("hidden");
+});
+
+img6.addEventListener('click', () => {
+    img5.classList.remove("hidden");
+});
+
+
+
